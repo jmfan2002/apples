d06d9505724d2885e9d0e5ce8e09e4d14bdbb64b
James Fan <jmfan2002@gmail.com>
2022-01-28 23:07:41 -0500

    better algo + run infinitely mode

--- src/main/java/com/example/apples/App.java
+++ src/main/java/com/example/apples/App.java
@@ -8,6 +8,7 @@ import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
@@ -23,14 +24,37 @@ public class App {
     static final int WHITE_THRESHOLD = 650;
 
     static int[][] apples;
-    static int size = 2;
+    static boolean RUN_FOREVER = true;
+    static int HIGHSCORE = 151;
+    static int scores = 0;
 
     public static void main(String[] args) throws Exception {
+        if (RUN_FOREVER) {
+            while (true) {
+                int score = runBot();
+                Robot bot = new Robot();
+                bot.delay(5000);
+                if (score > HIGHSCORE) {
+                    BufferedImage img = bot
+                            .createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                    ImageIO.write(img, "png", new File("./highscore" + scores + ".png"));
+                }
+            }
+        } else {
+            runBot();
+        }
+    }
+
+    // returns the final score
+    static int runBot() {
         getApples();
         int applesCleared = calculateMove();
+        int score = applesCleared;
         while (applesCleared > 0) {
             applesCleared = calculateMove();
+            score += applesCleared;
         }
+        return score;
     }
 
     static void getApples() {
@@ -49,8 +73,7 @@ public class App {
             bot.mousePress(mask);
             bot.mouseRelease(mask);
             bot.delay(500);
-            // take screenshot
-            // rectangle is set for the bounds of the apple box
+            // take screenshot, rectangle is set for the bounds of the apple box
             BufferedImage img = bot.createScreenCapture(new Rectangle(TOP_CORNER_X, TOP_CORNER_Y, 1270, 772));
             ImageIO.write(img, "png", new File("./screenshot.png"));
             // binarize
@@ -60,8 +83,7 @@ public class App {
                     final int red = (clr & 0x00ff0000) >> 16;
                     final int green = (clr & 0x0000ff00) >> 8;
                     final int blue = clr & 0x000000ff;
-                    // above white threshold and not green to white (cuz the apple numbers are
-                    // red-ish)
+                    // above white threshold and red-ish (cuz the apple numbers are red-ish)
                     if (red + green + blue > WHITE_THRESHOLD && green <= red && green <= blue) {
                         img.setRGB(x, y, new Color(255, 255, 255).getRGB());
                     } else {
@@ -102,8 +124,8 @@ public class App {
             int mask = InputEvent.BUTTON1_DOWN_MASK;
             bot.mouseMove(TOP_CORNER_X + col1 * APPLE_SIZE, TOP_CORNER_Y + row1 * APPLE_SIZE);
             bot.mousePress(mask);
-            // bot.delay(100);
             bot.mouseMove(TOP_CORNER_X + (col2 + 1) * APPLE_SIZE, TOP_CORNER_Y + (row2 + 1) * APPLE_SIZE);
+            // bot.delay(400);
             bot.mouseRelease(mask);
             // clear from data
             for (int i = 0; i <= row2 - row1; i++) {
@@ -118,40 +140,37 @@ public class App {
 
     // returns the number of apples cleared
     static int calculateMove() {
-        while (size < 11) {
-            for (int row = 0; row < apples.length; row++) {
-                for (int col = 0; col < apples[row].length; col++) {
-                    int sum = apples[row][col];
-                    // empty space
-                    if (sum == 0) {
-                        continue;
-                    }
-                    // go top-down
-                    int k = 1;
-                    while (row + k < apples.length && sum < 10 && k < size) {
-                        sum += apples[row + k][col];
-                        ++k;
-                    }
-                    if (sum == 10) {
-                        --k;
-                        performClick(row, col, row + k, col);
-                        return k + 1;
-                    }
-                    // go across the row
-                    sum = apples[row][col];
-                    k = 1;
-                    while (col + k < apples[row].length && sum < 10 && k < size) {
-                        sum += apples[row][col + k];
-                        k++;
-                    }
-                    if (sum == 10) {
-                        --k;
-                        performClick(row, col, row, col + k);
-                        return k + 1;
+        for (int row = 0; row < apples.length; row++) {
+            for (int col = 0; col < apples[row].length; col++) {
+                // checks every box that the mouse can make
+                for (int box_height = 1; box_height < apples.length - row + 1; box_height++) {
+                    for (int box_width = 1; box_width < apples[row].length - col + 1; box_width++) {
+                        // sum the apples in the box
+                        int count = 0;
+                        int sum = 0;
+                        for (int y = row; y < row + box_height; y++) {
+                            for (int x = col; x < col + box_width; x++) {
+                                sum += apples[y][x];
+                                if (apples[y][x] > 0) {
+                                    count++;
+                                }
+                                if (sum > 10) {
+                                    break;
+                                }
+                            }
+                            if (sum > 10) {
+                                break;
+                            }
+                        }
+                        if (sum == 10) {
+                            performClick(row, col, row + box_height - 1, col + box_width - 1);
+                            return count;
+                        } else if (sum > 10) {
+                            break;
+                        }
                     }
                 }
             }
-            size++;
         }
         return 0;
     }
